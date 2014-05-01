e0fdd5f762065f2001b65127ededb66763fa2df2
tenevdev <tenev.dev@outlook.com>
2014-05-01 11:36:54 +0300

    Create UserInfo control and update styles

---
+++ ClumsyWordsUniversal/ClumsyWordsUniversal.Shared/Common/Controls/UserInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ClumsyWordsUniversal.Controls
+{
+    public class UserInfo : Control
+    {
+        public UserInfo()
+        {
+            this.DefaultStyleKey = typeof(UserInfo);
+        }
+
+        public string FirstName
+        {
+            get { return (string)GetValue(FirstNameProperty); }
+            set { SetValue(FirstNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FirstName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FirstNameProperty =
+            DependencyProperty.Register("FirstName", typeof(string), typeof(UserInfo), new PropertyMetadata("Name"));
+
+
+
+        public string LastName
+        {
+            get { return (string)GetValue(LastNameProperty); }
+            set { SetValue(LastNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LastName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LastNameProperty =
+            DependencyProperty.Register("LastName", typeof(string), typeof(UserInfo), new PropertyMetadata("Name"));
+
+
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(UserInfo), new PropertyMetadata(0));
+
+    }
+}
