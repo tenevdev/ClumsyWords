41d6f421136b5f12d77ea3ec5fac0f933ae9c0ab
tenevdev <tenev.dev@outlook.com>
2014-05-08 10:18:04 +0300

    Create classes for view state managment

---
+++ ClumsyWordsUniversal/ClumsyWordsUniversal.Windows/Views/ViewStateManagment/CustomViewStates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClumsyWordsUniversal.Views.ViewStateManagment
+{
+    public class CustomViewStates
+    {
+        public string State { get; set; }
+
+        public Func<double, double, bool> MatchState { get; set; }
+    }
+}
---
+++ ClumsyWordsUniversal/ClumsyWordsUniversal.Windows/Views/ViewStateManagment/PageViewStateManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ClumsyWordsUniversal.Views.ViewStateManagment
+{
+
+    public class PageViewStateManager
+    {
+        private Page _page;
+
+        public PageViewStateManager(Page page)
+        {
+            this._page = page;
+            this._page.Loaded += Loaded;
+            this._page.Unloaded += Unloaded;
+        }
+
+        public IEnumerable<CustomViewStates> States { get; set; }
+
+        private void Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //Window.Current.SizeChanged -= WindowSizeChanged;
+        }
+
+        private void Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //Window.Current.SizeChanged += WindowSizeChanged;
+            DetermineState(Window.Current.Bounds.Width, Window.Current.Bounds.Height);
+        }
+
+        private void WindowSizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            DetermineState(e.Size.Width, e.Size.Height);
+        }
+
+        private void DetermineState(double width, double height)
+        {
+            var state = States.First(x => x.MatchState(width, height));
+
+            VisualStateManager.GoToState(this._page, state.State, false);
+        }
+
+    }
+}
